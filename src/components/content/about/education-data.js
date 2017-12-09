3656c28d7c94e1c23ec79160594890f8f3927e5e
TrRo26 <travis.roy26@gmail.com>
2017-12-09 16:36:24 -0600

    Add edu content

---
+++ src/components/content/about/education-data.js
@@ -0,0 +1,32 @@
+export function devBootcampEduData() {
+    var devBootcampEduData = {
+        eduInstitute: "DevBootcamp",
+        eduLocation: "Chicago, IL",
+        eduDegree: "Coding School Graduate in Full Stack Web Development",
+        eduDate: "June 2016 to December 2016",
+        EduDepartment: ""
+    }
+    return devBootcampEduData
+}
+
+export function stockholmUniData() {
+    var stockholmUniData = {
+        eduInstitute: "Stockholm University",
+        eduLocation: "Stockholm, Sweden",
+        eduDegree: "Master of Science in International and Comparative Education",
+        eduDate: "Aug 2012 to May 2014",
+        EduDepartment: "Department of Education"
+    }
+    return stockholmUniData
+}
+
+export function msuData() {
+    var msuData = {
+        eduInstitute: "Michigan State University",
+        eduLocation: "East Lansing, Michigan",
+        eduDegree: "Bachelor of Arts in Hospitality Business",
+        eduDate: "Aug 2000 to May 2005",
+        EduDepartment: "Eli Broad College of Business"
+    }
+    return msuData
+}
