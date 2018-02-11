a65b17d26169d11e6d198d26be5b6bb34b919637
TrRo26 <travis.roy26@gmail.com>
2018-02-11 10:07:16 -0600

    make header fully responsive

--- src/components/header/header-main.js
+++ src/components/header/header-main.js
@@ -1,21 +1,22 @@
 import React, { Component, Image } from 'react'
 import "./header-main.css"
-// import lofoten from "../../images/lofoten.jpg"
+import lofoten from "../../images/photos/lofoten.jpg"
 import NavLink from "../nav-link.js"
 
 class HeaderMain extends Component {
 
     render() {
         return(
-            <div className="header-frame">
+            <header className="header-frame">
+                <div className="overlay"></div>
+                <img src={lofoten} alt={"lofoten.jpg"} className="header-img"/>
                 <span className="me">TRAVIS A. ROY</span>
                 <div className="page-links">
                     <NavLink name={"about"} />
                     <NavLink name={"projects"} />
                     <NavLink name={"posts"} />
                 </div>
-                {/* <img src={lofoten} alt={"lofoten.jpg"} className="header-img"/> */}
-            </div>
+            </header>
         )
     }
 }
