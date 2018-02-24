1b413a4ff8bb99fbdbb05e268e97ae7d259f5284
TrRo26 <travis.roy26@gmail.com>
2018-02-24 11:03:15 -0600

    Add post grid basic layout

---
+++ src/components/content/posts/post.js
@@ -0,0 +1,19 @@
+// PACKAGES
+import React, { Component } from 'react'
+// CSS
+    import "./post.css"
+// IMAGES
+    // import tr_garmisch from "../../../images/photos/tr_garmisch.jpg"
+
+class Post extends Component {
+
+    render() {
+        return(
+            <div className="post-container">
+                POST
+            </div>
+        )
+    } 
+}
+
+export default Post
--- src/components/content/posts/posts-grid.js
+++ src/components/content/posts/posts-grid.js
@@ -3,15 +3,23 @@ import React, { Component } from 'react'
 // CSS
 import "./posts-grid.css"
 // IMAGES
-// import tr_garmisch from "../../../images/photos/tr_garmisch.jpg"
+    // import tr_garmisch from "../../../images/photos/tr_garmisch.jpg"
+// COMPONENTS
+import Post from "./post.js"
 
 class PostsGrid extends Component {
 
     render() {
         return(
-            <div>Dis Da Grid</div>
-
-
+            <div className="posts-grid-container">
+                <h3>Dis Da Grid</h3>
+                <div className="gridify">
+                    <Post />
+                    <Post />
+                    <Post />
+                    <Post />
+                </div>
+            </div>
         )
     } 
 }
